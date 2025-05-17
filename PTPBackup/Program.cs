33f06a39239e96b3d7e716583c2e4dd679917679
Arumoy Chakraborty <arumoy.chakraborty@delta.com>
2025-05-18 00:16:46 +0530

    motorola setup

--- PTPBackup/Program.cs
+++ PTPBackup/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace PTPBackup
+﻿using MediaDevices;
+
+namespace PTPBackup
 {
     internal class Program
     {
+        private static string BASE = "\\Internal shared storage\\DCIM\\Camera";
+        private static string TARGET = "C:\\Users\\449085\\Desktop\\TE";
+        //private static string TARGET = ".";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Hunt for phone
+            MediaDevice? phone = MediaDevice.GetDevices().ToList().Where(dev => dev.Description == "motorola edge 50 pro").FirstOrDefault();
+            try
+            {
+                if (phone != null)
+                {
+                    if (!phone.IsConnected)
+                    {
+                        phone.Connect();
+                    }
+                    // list files
+                    string[] files = phone.GetFiles(BASE);
+                    //foreach (var item in files)
+                    //{
+                    //    Console.WriteLine(item);
+                    //}
+                    Console.WriteLine(files.Length);
+                    string file = files[0];
+                    var parts = file.Split("\\");
+                    Console.WriteLine(parts[parts.Length - 1]);
+                    phone.DownloadFile(file, TARGET);
+                }
+            }
+            catch (NotConnectedException ncex)
+            {
+                Console.WriteLine(ncex.Message);
+            }
+            catch (UnauthorizedAccessException uex)
+            {
+                Console.WriteLine(uex.Message);
+            }
+            finally
+            {
+                if (phone != null)
+                {
+                    phone.Disconnect();
+                }
+            }
         }
     }
 }
